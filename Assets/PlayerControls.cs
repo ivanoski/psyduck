c614b766fc00def47d2db13ef10de3ec5b5ede84
ivanoski <ivanoski@hotmail.co.uk>
2023-07-28 18:28:58 +0100

    new controls, mouse click

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -11,6 +11,7 @@ public class PlayerControls : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundLayer;
 
+
     private bool isGrounded;
 
     private void Awake()
@@ -20,18 +21,28 @@ public class PlayerControls : MonoBehaviour
 
     private void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        //float moveInput = Input.GetAxis("Horizontal");
+        //rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        /*if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        } 
+        }*/
+
+        if (isGrounded && Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         
+            Vector2 directionToClick = mousePosition - new Vector2(transform.position.x, transform.position.y);
+            directionToClick.Normalize();
+            Debug.Log(directionToClick);
+            rb.AddForce(directionToClick * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
-    /*void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
         // Check if the player is grounded
         isGrounded = true;
@@ -41,5 +52,5 @@ public class PlayerControls : MonoBehaviour
     {
         // Check if the player leaves the ground
         isGrounded = false;
-    }*/
+    }
 }
